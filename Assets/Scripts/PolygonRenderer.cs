abebf54661b555e37dadff06618994bf68973d33
Victor Buldakov <victor@buldakov.org>
2019-09-30 16:32:34 +0100

    add mesh renderer and a second level

---
+++ Assets/Scripts/PolygonRenderer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class PolygonRenderer : MonoBehaviour
+{
+    public Material material;
+    public Vector2[] points;
+
+    public void UpdateMesh() {
+        // Use the triangulator to get indices for creating triangles
+        Triangulator tr = new Triangulator(points);
+        int[] indices = tr.Triangulate();
+
+        // Create the Vector3 vertices
+        Vector3[] vertices = new Vector3[points.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = new Vector3(points[i].x, points[i].y, 0);
+        }
+
+        // Create the mesh
+        Mesh msh = new Mesh
+        {
+            vertices = vertices,
+            triangles = indices
+        };
+        msh.RecalculateNormals();
+        msh.RecalculateBounds();
+
+        MeshFilter filter = gameObject.GetComponent<MeshFilter>();
+        if (filter == null)
+        {
+            filter = gameObject.AddComponent<MeshFilter>();
+        }
+        filter.mesh = msh;
+
+
+
+        PolygonCollider2D polyCollider = gameObject.GetComponent<PolygonCollider2D>();
+        if (polyCollider == null) {
+            polyCollider = gameObject.AddComponent<PolygonCollider2D>();
+        }
+        polyCollider.points = points;
+    }
+
+    void Start()
+    {
+        UpdateMesh();   
+    }
+}
---
+++ Assets/Scripts/UIInspectors/PolygonRendererInspector.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PolygonRenderer))]
+public class PolygonRendererInspector : Editor
+{
+    PolygonRenderer renderer;
+
+    void OnEnable()
+    {
+        renderer = (PolygonRenderer)target;
+    }
+
+    void OnSceneGUI()
+    {
+        if (renderer)
+        {
+            Vector3[] vertices = new Vector3[renderer.points.Length];
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                vertices[i] = new Vector3(renderer.points[i].x, renderer.points[i].y, 0);
+            }
+            Handles.DrawAAPolyLine(vertices);
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Handles.color = Color.red;
+                Handles.Label(vertices[i], "P:" + i);
+                renderer.points[i] = Handles.PositionHandle(vertices[i], Quaternion.identity);
+            }
+            
+        }
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+            if (renderer)
+            {
+                renderer.UpdateMesh();
+            }
+        }
+    }
+}
